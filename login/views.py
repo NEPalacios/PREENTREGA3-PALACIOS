f27f216b924eb6bb782881b2942ebe29e839b94d
Nicolas Palacios <palaciosnicolas94@gmail.com>
2024-05-23 19:01:29 -0300

    fix: authenticationForm en login/iniciar.html

--- login/views.py
+++ login/views.py
@@ -1,21 +1,40 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 
+# def iniciar(request):
+#     if request.method == "POST":
+#         username = request.POST['username']
+#         password = request.POST['password']
+#         user = authenticate(request, username=username, password=password)
+#         if user is not None:
+#             login(request, user)
+#             return redirect('tienda:index')
+#         else:
+#             return render(request, "login/iniciar.html", {"error": "Nombre de usuario o contraseña incorrectos"})
+#     else:
+#         return render(request, "login/iniciar.html")
+
+
 def iniciar(request):
-    if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('tienda:index')  # Redirige a la página de inicio después de iniciar sesión
-        else:
-            return render(request, "login/iniciar.html", {"error": "Nombre de usuario o contraseña incorrectos"})
+    if request.method == "GET":
+        return render(request, "login/iniciar.html", {"form": AuthenticationForm})
     else:
-        return render(request, "login/iniciar.html")
+        name = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(username=name, password=password)
+        if user is None:
+            return render(
+                request,
+                "login/iniciar.html",
+                {
+                    "form": AuthenticationForm,
+                    "error": "Nombre de usuario o contraseña incorrectos",
+                },
+            )
+
 
 def registro(request):
     if request.method == "GET":
@@ -31,14 +50,17 @@ def registro(request):
             try:
                 user = User.objects.create_user(
                     username=request.POST["username"],
-                    password=request.POST["password1"]
+                    password=request.POST["password1"],
                 )
                 user.save()
-                login(request, user)  # Inicia sesión automáticamente al usuario después de registrarse
-                return redirect('tienda:index')  # Redirige a la página de inicio después del registro
+                login(request, user)
+                return redirect("tienda:index")
             except IntegrityError:
                 return render(
                     request,
                     "login/registro.html",
-                    {"form": UserCreationForm(), "error": "El nombre de usuario ya existe"},
+                    {
+                        "form": UserCreationForm(),
+                        "error": "El nombre de usuario ya existe",
+                    },
                 )
