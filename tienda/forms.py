48d54f6fadffca34caafa544dd9e057370fea5e6
Nicolas Palacios <palaciosnicolas94@gmail.com>
2024-06-09 12:30:42 -0300

    fix: PedidosForm

--- tienda/forms.py
+++ tienda/forms.py
@@ -1,6 +1,22 @@
 from django import forms
-from .models import Productos, Clientes
+from .models import Clientes, Productos
+
 
 class PedidosForm(forms.Form):
-    cliente = forms.ModelChoiceField(queryset=Clientes.objects.all(), label="Seleccione un Cliente")
-    productos = forms.ModelMultipleChoiceField(queryset=Productos.objects.all(), label="Seleccione Productos")
+    cliente = forms.ModelChoiceField(
+        queryset=Clientes.objects.all(),
+        label="Seleccione un Cliente",
+        to_field_name="id",
+    )
+    productos = forms.ModelMultipleChoiceField(
+        queryset=Productos.objects.all(),
+        label="Seleccione Productos",
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    def __init__(self, *args, **kwargs):
+        super(PedidosForm, self).__init__(*args, **kwargs)
+        self.fields["cliente"].label_from_instance = self.get_cliente_label
+
+    def get_cliente_label(self, cliente):
+        return f"{cliente.id} - {cliente.nombre}"
